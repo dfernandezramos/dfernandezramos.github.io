e08751fc0b3ef2785deec6974944358fd9c6a30a
dfernandezramos <dfernandez@fluendo.com>
2020-07-09 10:28:46 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,6 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
 # Go To Public folder
 cd public
 
@@ -19,6 +16,15 @@ git checkout master
 
 # Reset master to the upstream level
 git reset --hard origin/master
+
+# Get back to root folder
+cd ..
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
 
 # Add changes to git.
 git add .
